3381da21297bd3d11ba66e30aadbf5162ef22591
Dffffs <lilikang110@sina.com>
2018-08-01 09:03:56 +0800

    - 第四章 - 32

--- meadowlark.js
+++ meadowlark.js
@@ -1,17 +1,24 @@
 let express = require('express');
+let fs = require('fs');
 let handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+let fortune = require('./lib/fortune.js');
 let app = express();
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT||3000);
 app.use(express.static(__dirname + '/public'))
 
+fs.readFile(__dirname + '/public/about.html',(err,data)=>{
+    console.log(data.toString())
+});
+
 app.get('/',(req,res)=>{
     res.render('home')
 });
 
+
 app.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about',{fortune:fortune.getFourtune()});
 });
 
 app.use((req,res)=>{
@@ -28,4 +35,4 @@ app.use((err,req,res,next)=>{
 
 app.listen(app.get('port'),()=>{
     console.log('express started')
-})
+});
